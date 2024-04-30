f6cc4f3292ca403aa78c3ae86251ea78070c255d
sakika <takwit@gmail.com>
2024-04-30 23:12:40 +0000

    グラフ作成

--- front/app/components/Graph.tsx
+++ front/app/components/Graph.tsx
@@ -1,8 +1,8 @@
-import React from "react"
-import { useEffect } from "react"
+import React, { useEffect } from "react"
+import { Line, LineChart, XAxis, YAxis } from "recharts"
 
 type Composition = {
-  label: string
+  label?: string
   data: {
     year: number
     value: number
@@ -12,11 +12,58 @@ type PopulationComposition = {
   boundaryYear: number
   data: Composition[]
 }
-type Population = Composition & {
+type Population = {
   prefCode: number
   prefName: string
+} & {
+  label?: string
+  data?: {
+    year: number
+    value: number
+  }[]
 }
 
+type Props = {
+  population: Population[]
+}
+const Chart: React.FC<Props> = ({population}) => {
+  const [populationData, setPopulationData] = React.useState<Population[]>([])
+
+  useEffect(() => {
+    const years = population.map((p) => p.data?.map((d) => d.year)).flat()
+
+    const names = Array.from(new Set(years)).map((year) => {
+      return { name: year }
+    })
+
+    const populationData = names.map((n) => {
+      const data = population.map((p) => {
+        const d = p.data?.find((d) => d.year === n.name)
+        return { [p.prefName]: d ? d.value : 0 }
+      })
+      return { name: n.name, ...Object.assign({}, ...data) }
+    })
+    setPopulationData(populationData)
+    console.log(populationData)
+  }, [population])
+
+  return (
+    <>
+      <LineChart width={400} height={400} data={populationData}>
+        {population.map((p) => (
+          <Line
+            key={p.prefName}
+            type="monotone"
+            dataKey={p.prefName}
+            stroke="#8884d8"
+          />
+        ))}
+        <XAxis dataKey="name" />
+        <YAxis />
+      </LineChart>
+    </>
+  )
+}
 export const Graph = (selectedPrefs: pref[]) => {
   const [population, setPopulation] = React.useState<Population[]>([])
 
@@ -40,22 +87,21 @@ export const Graph = (selectedPrefs: pref[]) => {
 
   useEffect(() => {
     (async () => {
-      Object.keys(selectedPrefs).map(async (k) => {
-        const v = selectedPrefs[parseInt(k)]
-        const res = await req(v.prefCode, v.prefName)
-        // populationに重複するデータがあれば上書き
-        if (population.some((p) => p.prefCode === res.prefCode)) {
-          setPopulation((prev) => prev.map((p) => p.prefCode === res.prefCode ? res as Population : p))
-        } else {
-          setPopulation((prev) => [...prev, res as Population])
-        }
-      })
+      const populationData = await Promise.all(
+        Object.keys(selectedPrefs).map(async (k) => {
+          const v = selectedPrefs[parseInt(k)]
+          const res = await req(v.prefCode, v.prefName)
+          return res
+        })
+      )
+      setPopulation(populationData)
     })()
   }, [selectedPrefs])
 
   return (
     <>
       <p>Graph</p>
+      <Chart population={population} />
     </>
   )
 }
